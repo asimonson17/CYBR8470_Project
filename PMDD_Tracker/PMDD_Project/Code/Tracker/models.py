b2a482347f26ddea0d26734f8a5e6a506f576834
Alexandria Simonson <alexandriasimonson@unomaha.edu>
2023-10-26 18:38:32 -0500

    Added ember directory. Created base HTML project in static folder directoy. Added CSS styling to the app.css file. Added main page to the application.hbs file.

--- PMDD_Tracker/PMDD_Project/Code/Tracker/models.py
+++ PMDD_Tracker/PMDD_Project/Code/Tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+# Value Validatators to check integer values for input validation
 # Create your models here.
+
+class PMDD(models.Model):
+    name = models.CharField(max_length=30, null=True, blank=False)
+    date = models.IntegerField(validators=[
+            MinValueValidator(0),
+            MaxValueValidator(99)], null=True, blank=False)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default=MALE)
+    color = models.CharField(max_length=30, null=True, blank=False)
+    favoritefood = models.CharField(max_length=30, null=True, blank=False)
+    favoritetoy = models.CharField(max_length=30, null=True, blank=False)
+    #linenos = models.BooleanField(default=False)
+    #language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    #style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+      
+    def __str__(self):
+        return str(self.id) + " - " + self.name  
