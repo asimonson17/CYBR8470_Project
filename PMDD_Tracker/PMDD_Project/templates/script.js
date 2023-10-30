d9e966eba82d54be0c486e9b433821e5981ff331
Alexandria Simonson <alexandriasimonson@unomaha.edu>
2023-10-30 17:04:59 -0500

    added a graph with javascript to overview section of the calendar.html, only a dummy version. Need to figure out how to get it to pull actual months days instead of a static 1-31 as well as pull data for the mood ranges from the add entry form.

--- PMDD_Tracker/PMDD_Project/templates/script.js
+++ PMDD_Tracker/PMDD_Project/templates/script.js
@@ -79,3 +79,29 @@ document.querySelector('.next').addEventListener('click', () => {
 
 
 renderCalendar();
+
+
+const xValues = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]; 
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{ 
+      data: [5,2,4,1,10,9,2,1,10,2,5,2,4,1,10,9,2,1,10,2,5,2,4,1,10,9,2,1,10,2,5],
+      borderColor: "red",
+      fill: false
+    }, { 
+      data: [4,6,4,8,3,7,4,8,3,7,4,6,4,8,3,7,4,8,3,7,4,6,4,8,3,7,4,8,3,7,6],
+      borderColor: "green",
+      fill: false
+    }, { 
+      data: [8,1,9,4,1,4,9,4,1,4,9,8,1,9,4,1,4,9,4,1,4,9,8,1,9,4,1,4,9,4,1,4,9],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
+});
