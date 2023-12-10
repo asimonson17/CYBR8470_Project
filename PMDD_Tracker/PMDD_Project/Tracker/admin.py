668a942ecb7999d7ea7cb795916195afef8a4f56
Alexandria Simonson <alexandriasimonson@unomaha.edu>
2023-12-10 15:30:08 -0600

    flushed DB and added superuser back in to fix integrity issues. Added trackers and users view to admin console via the admin.py file

--- PMDD_Tracker/PMDD_Project/Tracker/admin.py
+++ PMDD_Tracker/PMDD_Project/Tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from Tracker.models import Tracker, User
+
+
+class TrackerAdmin(admin.ModelAdmin):
+    pass
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Tracker, TrackerAdmin)
 
 # Register your models here.
+
+
